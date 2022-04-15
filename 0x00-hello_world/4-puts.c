8635f53388a110f846f1b8050f7a7c2beb6139e0
josephmaxime <mangajm004@gmail.com>
2022-04-15 07:07:51 -0700

    puts in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-int main()
+/*
+ *main - Display a message with puts
+ *
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
