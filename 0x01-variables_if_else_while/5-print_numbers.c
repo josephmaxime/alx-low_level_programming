435c3c982d17055fc39453532a2025bf350e3508
josephmaxime <mangajm004@gmail.com>
2022-04-16 08:14:01 -0700

    print number in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+/**
+ * main - Entry code
+ *
+ * Return:  0 IN THE END
+ */
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+
+}
