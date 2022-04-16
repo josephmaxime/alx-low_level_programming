3505c5d35b53d9f0665259d598466cb299ebcd81
josephmaxime <mangajm004@gmail.com>
2022-04-16 07:28:18 -0700

    print a part of alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+/**
+ * main - Entry code
+ *
+ * Return:  0 IN THE END
+ */
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' || letter != 'e')
+			putchar(letter);
+		else
+			continue;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
