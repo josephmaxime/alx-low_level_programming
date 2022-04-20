3b5bc08c335d27241ed50107409d426ebeac1a30
josephmaxime <mangajm004@gmail.com>
2022-04-20 14:32:43 -0700

    test positve or negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry code
+ *
+ * Return: 0 Always for success
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* positive_or_negative */
+void positive_or_negative(int);
+
+#endif
