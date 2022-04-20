7a6a3069c7b8bb6a8a72e41d9c0a03c8b9642eb6
josephmaxime <mangajm004@gmail.com>
2022-04-20 15:42:53 -0700

    remaining day

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,4 +9,7 @@ void positive_or_negative(int i);
 /* largest_number */
 int largest_number(int, int, int);
 
+/* print remainning days */
+void print_remaining_days(int, int, int);
+
 #endif /* MAIN_H */
