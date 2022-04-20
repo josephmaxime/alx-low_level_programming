a3364c685909ee952860d86a0b40bd3d2bb7284c
josephmaxime <mangajm004@gmail.com>
2022-04-20 16:00:22 -0700

    test positive or negative function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - Entry code
- *
- * Return: 0 Always for success
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 int main(void)
 {
