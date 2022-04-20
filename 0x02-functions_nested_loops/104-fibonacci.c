894bb275d8c0eceb94cb9ae7ae56afdbc0a14ef7
josephmaxime <mangajm004@gmail.com>
2022-04-20 11:21:27 -0700

    fibonacci 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#include <string.h>
+
+#include <ctype.h>
+
+#include <stdlib.h>
+
+#include "main.h"
+
+/**
+ * main - fibonacci(98)
+ *
+ * Return: 0 always for success
+ */
+
+int main(void)
+{
+	int i;
+	long init_1, init_2, next;
+
+	init_1 = 1;
+	init_2 = 2;
+	for (i = 0; i < 98; i++)
+	{
+		if (i == 0)
+		{
+			next = init_1;
+			printf("%ld, ", next);
+		}
+		else if (i == 1)
+		{
+			next = init_2;
+			printf("%ld, ", next);
+		}
+		else
+		{
+			if (i < 97)
+			{
+				next = init_1 + init_2;
+				init_1 = init_2;
+				init_2 = next;
+				printf("%ld, ", next);
+			}
+			else
+			{
+				next = init_1 + init_2;
+				init_1 = init_2;
+				init_2 = next;
+				printf("%ld", next);
+			}
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
