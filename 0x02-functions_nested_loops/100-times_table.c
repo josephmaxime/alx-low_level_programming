297f4addf301797d6ee371897ea619c1d8fd61a0
josephmaxime <mangajm004@gmail.com>
2022-04-20 06:27:27 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 					}
 					else
 					{
-					printf("%d,  ", res);
+						printf("%d, ", res);
 					}
 				}
 				else
@@ -51,6 +51,6 @@ void print_times_table(int n)
 	}
 	else
 	{
-
+		continue;
 	}
 }
