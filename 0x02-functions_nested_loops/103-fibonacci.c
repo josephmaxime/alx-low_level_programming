65649c104aaa8a2ef0a74ca9b021c680c8760a5a
josephmaxime <mangajm004@gmail.com>
2022-04-20 10:23:29 -0700

    sum fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include <string.h>
+
+#include <ctype.h>
+
+#include <stdlib.h>
+
+#include "main.h"
+
+/**
+ * main - display sum of the natural numbers multiples of 3 or 5 below 1024
+ *
+ * Return: 0 always for success
+ */
+
+int main(void)
+{
+	int i, mod_2;
+	long init_1, init_2, next, sum;
+
+	init_1 = 1;
+	init_2 = 2;
+	sum = 2;
+	next = 0;
+	for (i = 2; i < 4000000; i++)
+	{
+		next = init_1 + init_2;
+		init_1 = init_2;
+		init_2 = next;
+		mod_2 = next % 2;
+		if (mod_2 == 0)
+		{
+			sum += next;
+		}
+	}
+	
+	printf("%ld", sum);
+	printf("\n");
+	return (0);
+}
