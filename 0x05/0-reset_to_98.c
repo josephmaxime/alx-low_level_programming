3736c007ceee3d3980e1021c0f743df9068be2da
josephmaxime <mangajm004@gmail.com>
2022-04-22 02:20:59 -0700

    task 0x05-p

---
+++ 0x05/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - change value of poiter to 98
+ *
+ * @n: pointer
+ * Return: 0 Always in success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
