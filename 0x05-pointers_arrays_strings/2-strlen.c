1aa149571146f01e1dca96b68789102129a12c6e
josephmaxime <mangajm004@gmail.com>
2022-04-22 03:22:15 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - change value of poiter to 98
+ *
+ * @s: pointer
+ * Return: int value of length
+ */
+
+int _strlen(int *s)
+{
+	strlen(s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,4 +10,7 @@ void reset_to_98(int *n);
 /* swap int */
 void swap_int(int *a, int *b);
 
+/* _strlen */
+int _strlen(int *s);
+
 #endif /* MAIN_H */
