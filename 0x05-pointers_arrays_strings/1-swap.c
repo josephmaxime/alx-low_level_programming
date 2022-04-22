1f00da375e4697ac60441edb2ef0ced0324aca01
josephmaxime <mangajm004@gmail.com>
2022-04-22 03:03:19 -0700

    swap integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * swap_int - change value of poiter to 98
+ * @a: pointer
+ * @b: pointer
+ * Return: 0 Always in success
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *swap;
+
+	*swap = *a;
+	*a = *b;
+	*b = *swap;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,11 @@
 
 /* _putchar */
 int _putchar(char);
+
 /* reset to 98 */
 void reset_to_98(int *n);
+
+/* swap int */
+void swap_int(int *a, int *b);
+
 #endif /* MAIN_H */
