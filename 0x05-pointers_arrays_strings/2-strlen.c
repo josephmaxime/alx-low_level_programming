d7dcf7cb7bda2d64307dd1cf96c89457a5ff4559
josephmaxime <mangajm004@gmail.com>
2022-04-22 10:21:42 -0700

    length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - change value of poiter to 98
+ * _strlen - change value of poiter to 98
  *
  * @s: pointer
  * Return: int value of length
@@ -12,5 +12,9 @@
 
 int _strlen(char *s)
 {
-	strlen(s);
+	int length;
+
+	length = strlen(s);
+
+	return (length);
 }
