284d2eca9036c3412f163fb18a3c5e4c85463397
josephmaxime <mangajm004@gmail.com>
2022-04-22 18:06:30 -0700

    convert string to iteger

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,12 +19,12 @@ int _atoi(char *s)
 	length = strlen(s);
 	sign = 1;
 	for (i = 0; i < length; i++)
-		if (!isdigit(s[i]) && s[i] == '-')
+		if (!(isdigit(s[i])) && s[i] == '-')
 			sign = -1 * sign;
 			sign *= sign;
-		else if (!isdigit(s[i]) && s[i] == '+')
+		else if (!(isdigit(s[i])) && s[i] == '+')
 			sign *= sign;
 		else
-			val += atoi(s[i]) + '0';
+			val += (atoi(s[i]) + '0');
 	return (sign * atoi(val));
 }
