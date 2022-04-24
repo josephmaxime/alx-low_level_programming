2d68c76fdabc0a0587b61c5bf7cea14dabf77bee
josephmaxime <mangajm004@gmail.com>
2022-04-24 11:27:44 -0700

    concatenate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: destination
+ * @src: source
+ *
+ * Return: string of concatenate
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar */
+int _putchar(char);
+
+/* _strcat */
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
