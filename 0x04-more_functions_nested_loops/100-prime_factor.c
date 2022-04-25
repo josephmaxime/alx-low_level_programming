5c0c167f6de62fbb9e5dc435b24d32508ca106af
josephmaxime <mangajm004@gmail.com>
2022-04-25 04:16:04 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - checks if is lowercase
+ *
+ * Return: 1 if c is lowercase
+ *
+ */
+
+int main(void)
+{
+	int i, j, isPrime;
+
+	for (i = 2; i <= 612852475143; i++)
+	{
+		/* Check 'i' for factor of num */
+		if (num % i == 0)
+		{
+			/* Check 'i' for Prime */
+			isPrime = 1;
+			for (j = 2; j <= i / 2; j++)
+			{
+				if ((i % j) == 0)
+				{
+					isPrime = 0;
+					break;
+				}
+			}
+			/* If 'i' is Prime number and factor of num */
+			if (isPrime == 1)
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	return (0);
+}
