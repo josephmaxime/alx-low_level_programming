b2c25fcb3f8fed7c22f77f77faa764d3afaf2f14
josephmaxime <mangajm004@gmail.com>
2022-04-25 11:00:08 -0700

    print number with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,8 +18,9 @@ void print_number(int n)
 		putchar('-');
 		n = -n;
 	}
-	else
-		if (n / 10)
-			print_number(n / 10);
+	if (n == 0)
+		putchar('0');
+	if (n / 10)
+		print_number(n / 10);
 	putchar(n % 10 + '0');
 }
