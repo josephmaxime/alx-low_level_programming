71a80cf5af3b1c11d82fa1ae44a144fda124847b
josephmaxime <mangajm004@gmail.com>
2022-04-25 03:01:34 -0700

    print two combination of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ *
+ * Return: integer
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n < 99; n++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (m > n)
+			{
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				putchar(' ');
+				putchar((m / 10) + '0');
+				putchar((m % 10) + '0');
+			}
+			if (n < 98 && (m > n))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
