fc09b9d402f2e458b5e1a2bb9970ed643b88ecc6
josephmaxime <mangajm004@gmail.com>
2022-04-27 09:41:04 -0700

    copy memory

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,5 @@ int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 
 /* copy memory */
-char *memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif /* MAIN_H */
