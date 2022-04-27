60fc9cbbafef0661fb0a74cf6427ec2f7e0a99bb
josephmaxime <mangajm004@gmail.com>
2022-04-27 07:52:29 -0700

    fill memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _memset - fill memory with a constatnt byte
+ *
+ * Return: pointers of char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* putchar function */
+int _putchar(char);
+
+/* memset fill memory */
+char *_memset(char *s, char b, unsigned int n);
