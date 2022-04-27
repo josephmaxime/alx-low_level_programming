8c8180c2bb62bbdff9bddec04d980d8452e0d02d
josephmaxime <mangajm004@gmail.com>
2022-04-27 08:01:35 -0700

    fill memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,10 +6,20 @@
 /**
  * _memset - fill memory with a constatnt byte
  *
+ * @s:memory to fill
+ * @b: char
+ * @n: number of bit
+ *
  * Return: pointers of char
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	return (memset(s, b, n);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b
+	}
+	return (s);
 }
