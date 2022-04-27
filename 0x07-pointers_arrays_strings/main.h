2757822177e20742061c7a69046ec1b6c31dde31
josephmaxime <mangajm004@gmail.com>
2022-04-27 10:01:34 -0700

    find char in string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,8 @@ char *_memset(char *s, char b, unsigned int n);
 
 /* copy memory */
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* search char in string */
+char *_strchr(char *s, char c);
+
 #endif /* MAIN_H */
