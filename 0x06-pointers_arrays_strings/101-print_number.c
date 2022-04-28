b5f09cb2f0b22adf924f4129a7bf06a6b8695c56
josephmaxime <mangajm004@gmail.com>
2022-04-28 15:26:01 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -21,10 +21,16 @@ void print_number(int n)
 	if (n == 0)
 		;
 	if (n <= 10)
+	{
 		 print_number(n / 10);
+	}
 	else if (n >= 10 && n <= 100)
+	{
 		 print_number(n / 100);
+	}
 	else (n >= 100 && n <= 1000)	
-		 print_number(n / 1000);
+	{
+		print_number(n / 1000);
+	}
 	putchar(n % 10 + '0');
 }
