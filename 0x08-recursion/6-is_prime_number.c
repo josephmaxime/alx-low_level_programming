8f952cf687b09a653260bb6446b9fa579523f6ea
josephmaxime <mangajm004@gmail.com>
2022-04-28 19:08:42 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -36,7 +36,7 @@ int is_prime_number(int n)
 		return (0);
 	if (n == 2)
 		return (1);
-	if (2more_than_two_divider(n, 2, n / 2))
+	if (more_than_two_divider(n, 2, n / 2))
 		return (0);
 	return (1);
 }
