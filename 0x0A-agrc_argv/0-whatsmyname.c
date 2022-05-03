42e1b143c27febf881245c31487048dabb81523c
josephmaxime <mangajm004@gmail.com>
2022-05-03 07:30:39 -0700

    whatsmyame

---
+++ 0x0A-agrc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry code
+ *
+ * @argc: number of arguments
+ * @argv: array of string of arguments
+ *
+ * Return: 0 for always success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
