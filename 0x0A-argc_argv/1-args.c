d16208f1b95f9492870bdc3dcc71ff2b293c5691
josephmaxime <mangajm004@gmail.com>
2022-05-03 08:34:32 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry code
+ *
+ * @argc: number of parameter
+ * @argv: array of string of parameter
+ *
+ * Return: 0 for always success
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
