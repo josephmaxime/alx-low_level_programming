428ebd346a94d9b7fd37f0e3e4ec1f72dfe43e26
josephmaxime <mangajm004@gmail.com>
2022-05-06 11:32:35 -0700

    malloc size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	int i;
 
 	str = malloc(size);
 	str[0] = c;
+	if (size == 0)
+		return (NULL);
+	str = malloc(sizeof(*str) * size);
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
 	return (str);
 }
