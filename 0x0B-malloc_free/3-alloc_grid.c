95683dea8160393cbb64a4797eb12f4766d6419f
josephmaxime <mangajm004@gmail.com>
2022-05-07 12:45:56 -0700

    array of two dimensions

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,14 +20,18 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	if (width < 0 || height < 0)
 		return (NULL);
-	array = malloc(sizeof(**array) * height);
-	for (i = 0; i < height; i++)
-	{
-		array[i] = malloc(sizeof(*array) * width);
-		free(array[i]);
-	}
+	array = malloc(sizeof(*int) * height);
 	if (array == NULL)
 		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+		if (array[i] = NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(array[i]);
+		}
+	}
 	free(array);
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
