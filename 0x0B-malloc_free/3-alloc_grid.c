2b8f1f0c27b6076cee050fb8acffbacec60d1f59
josephmaxime <mangajm004@gmail.com>
2022-05-07 09:17:18 -0700

    array of two dim

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - create an array of two dimensions
+ *
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: array [][]
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **array;
+
+	array = (int *)malloc(width * sizeof(*array));
+	for (i = 0; i < width; ++i)
+		array[i] = (int *)malloc(height * sizeof(**array));
+	if (array == NULL)
+		return (NULL);
+	if (width == 0 || height == 0)
+		return (NULL);
+	if (width < 0 || height < 0)
+		return (NULL);
+	for (i = 0; i < width; i++)
+		for (j = 0; j < height; j++)
+			array[i][j] = 0;
+	return (array);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -12,4 +12,7 @@ char *_strdup(char *str);
 
 /* str_concat prototype */
 char *str_concat(char *s1, char *s2);
+
+/* prototype alloc grid */
+int **alloc_grid(int width, int height);
 #endif /* MAIN_H */
