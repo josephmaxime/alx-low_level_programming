b9f403035cd03d180d10dbe955c8e2443efe4dab
josephmaxime <mangajm004@gmail.com>
2022-05-10 15:01:29 -0700

    print name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry code
+ *
+ * @argc: number of parameter
+ * @argv: array of parameter
+ *
+ * Return: 0 Always true
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	length = strlen(argv[0]);
+	for (i = 0; i < length; i++)
+		putchar(argv[0][i]);
+	return (0);
+}
