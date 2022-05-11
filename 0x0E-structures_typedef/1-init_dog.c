7ce64e3d7dc1d881db4b56b76e45e9cda9fc0df9
josephmaxime <mangajm004@gmail.com>
2022-05-11 08:18:58 -0700

    init structure dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * init_dog - init structure of dog
+ * @d: structure dog
+ * @name: name to init name of dog
+ * @age: age to init age of dog
+ * @owner: owner to init owner of dog
+ *
+ */
 void init_dog(struct dog *d, char *name,float age, char *owner)
 {
 	(*d).name = name;
