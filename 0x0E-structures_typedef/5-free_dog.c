86889cdbb2adfc34f02541887ce01dccd60e4a83
josephmaxime <mangajm004@gmail.com>
2022-05-11 14:14:44 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * free_dog - create a new dog
+ * @d: structure of dog
+ * 
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+	return (d);
+}
+
