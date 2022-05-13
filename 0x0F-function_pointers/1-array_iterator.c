8f343c69b1122c613838596b4a3c9b6d7d64e385
josephmaxime <mangajm004@gmail.com>
2022-05-13 15:17:13 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array != NULL && action != NULL)
 	{
-		size = strlen(array);
 		for (i = 0; i < size; i++)
 		{
 			(*action)(array[i]);
