b6156bab90238f93df1c156d5ff4e1ea75ad0cb7
josephmaxime <mangajm004@gmail.com>
2022-05-13 15:52:52 -0700

    search int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,8 +8,10 @@
  * @array: array of arg to execute
  * @size: size of array
  * @cmp: pointer to a function you be use to compare values
+ *
+ * Return: index for true and -1 otherwise
  */
-int array_iterator(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
@@ -17,7 +19,7 @@ int array_iterator(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if ((*cmp)(array[i]) != 0) 
+		if ((*cmp)(array[i]) != 0)
 			return (i);
 	}
 	return (-1);
