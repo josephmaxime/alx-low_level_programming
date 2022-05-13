39d79483e34deaaa2413e48273ebfbde27614a3f
josephmaxime <mangajm004@gmail.com>
2022-05-13 15:10:40 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function of array of function
+ *
+ * @array: array of arg to execute
+ * @size: size of array
+ * @action: pointer to a function you need to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && action != NULL)
+	{
+		size = strlen(array);
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,4 +3,7 @@
 #include <stddef.h>
 /* prototype of print_name */
 void print_name(char *name, void (*f)(char *));
+
+/* prototype array iterator */
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
