58c78379f6e92bc81a9df063ec719a4bbe154aed
josephmaxime <mangajm004@gmail.com>
2022-05-13 14:21:47 -0700

    print the name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
-/* prototyype of print_name */
+#include <stddef.h>
+/* prototype of print_name */
 void print_name(char *name, void (*f)(char *));
 #endif
