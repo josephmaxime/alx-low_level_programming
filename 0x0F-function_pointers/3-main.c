729772611631ad3f35c20145730ae97e2a35659f
josephmaxime <mangajm004@gmail.com>
2022-05-15 14:45:26 -0700

    calculate with own operator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	number_1 = atoi(argv[1]);
 	number_2 = atoi(argv[3]);
 	op = argv[2];
-	if (get_op_func(op) == NULL || op[1] != '\0' || op == ".")
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 		case '%':
 			printf("%d\n", get_op_func(argv[2])(number_1, number_2));
 			break;
-		default:
+		case '.':
 		printf("Error\n");
 		exit(99);
 	}
