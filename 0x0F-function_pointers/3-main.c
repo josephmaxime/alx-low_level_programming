0ded1116001d99ac1b7196da9f6c5b12623359d1
josephmaxime <mangajm004@gmail.com>
2022-05-15 14:33:51 -0700

    calculate with own operator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,11 +16,15 @@ int main(int argc, char *argv[])
 	int  number_1, number_2;
 	char *op;
 	/* char op_b[] = {'+','-','*','/','%'}; */
-
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	number_1 = atoi(argv[1]);
 	number_2 = atoi(argv[3]);
 	op = argv[2];
-	if (get_op_func(op) == NULL || op[1] != '\0' || argc != 4)
+	if (get_op_func(op) == NULL || op[1] != '\0' || op[0] == '.')
 	{
 		printf("Error\n");
 		exit(99);
@@ -36,7 +40,7 @@ int main(int argc, char *argv[])
 		case '-':
 		case '*':
 		case '/':
-		case '%':	
+		case '%':
 			printf("%d\n", get_op_func(argv[2])(number_1, number_2));
 			break;
 		default:
