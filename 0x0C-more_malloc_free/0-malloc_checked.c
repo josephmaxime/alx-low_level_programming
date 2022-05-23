c1e710e77d8800cacda9505cf885137ea83d36d7
josephmaxime <mangajm004@gmail.com>
2022-05-23 15:14:49 -0700

    add files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - return pointer to allocated memory
+ *
+ * @b: size to allocated
+ * Return: pointer to allocated for true or 98 for fail.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	size_t *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit (98);
+	return (ptr);
+}
