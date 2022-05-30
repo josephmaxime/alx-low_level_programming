f6b55b5836333cde66112806fc123a234812124b
josephmaxime <mangajm004@gmail.com>
2022-05-29 20:08:22 -0700

    add files

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * string_nconcat - return pointer to allocated to the newly allocate memory
+ *
+ * @n: size of string
+ * @s2: string
+ * @s1: string
+ * Return: pointer to allocated for contain s1 and s2.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int length, length2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	length = strlen(s1);
+	length2 = strlen(s2);
+	ptr = malloc(length + n + 1);
+	if (ptr == NULL)
+		return (NULL);
+	if (n >= length2)
+	{
+		strcat(ptr, s1);
+		strcat(ptr, s2);
+	}
+	else
+	{
+		strcat(ptr, s1);
+		strncat(ptr, s2, n);
+	}
+	return (ptr);
+}
