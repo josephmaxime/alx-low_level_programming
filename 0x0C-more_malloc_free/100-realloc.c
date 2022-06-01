314b00e06558db8bc82f45691fb632ad24f7734d
josephmaxime <mangajm004@gmail.com>
2022-05-31 18:33:24 -0700

    add files

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,8 +18,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i, mem;
 
 	if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
 		return (NULL);
+	}
 
 	if (ptr == NULL)
 	{
