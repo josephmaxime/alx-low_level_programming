f9139cb44c3219215aa85c1d8f5dcbcc136808d3
josephmaxime <mangajm004@gmail.com>
2022-05-31 19:00:04 -0700

    fix errors

---
+++ 0x12-singly_linked_lists/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	printf("test premain");
+	return (0);
+}
