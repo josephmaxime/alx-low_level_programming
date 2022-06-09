eec5b6ffe904d1cf0799e64e95b92558176c0ec3
josephmaxime <mangajm004@gmail.com>
2022-06-09 01:25:50 -0700

    add files

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * clear_bit - set a bit of the number biggin at 0 to index .
+ *
+ * @n: pointer of integer.
+ * @index: index of a bit.
+ * Return: 1 for true and 0 otherwise.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int init;
+
+	if (index >= (sizeof(*n) * 8))
+		return (-1);
+
+	init = 254 << index;
+	init |= 1;
+	*n = *n & init;
+	return (1);
+}
