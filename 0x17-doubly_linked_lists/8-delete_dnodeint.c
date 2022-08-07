9ddd7e5c1e49250b014a284c3ee755b43c976844
josephmaxime <mangajm004@gmail.com>
2022-08-07 14:41:16 -0700

    add

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	current = *head
+	current = *head;
 	if (index == 0)
 	{
 		*head = current->next;
